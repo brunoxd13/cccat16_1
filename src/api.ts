323d209c8948028b2c9028da8357e35a3c748b15
Bruno Russi Lautenschlager <brunoxd13@gmail.com>
2024-04-07 16:58:52 -0300

    refactor: extract validation methods

--- src/api.ts
+++ src/api.ts
@@ -1,13 +1,13 @@
 import crypto from "crypto";
 import express from "express";
 import pgp from "pg-promise";
-import { validate } from "./validateCpf";
+import { validate as validateCpf } from "./validateCpf";
 const app = express();
 app.use(express.json());
 
 app.post("/signup", async function (req, res) {
-  let result;
   const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
+	const { name, email, cpf, carPlate, isPassenger, isDriver } = req.body;
 
   try {
     const id = crypto.randomUUID();
@@ -18,55 +18,46 @@ app.post("/signup", async function (req, res) {
     );
 
     if (acc) return res.status(422).send("-4");
-		if (!req.body.name.match(/[a-zA-Z] [a-zA-Z]+/)) return res.status(422).send("-3");
-		if (!req.body.email.match(/^(.+)@(.+)$/)) return res.status(422).send("-2");
-		if (!validate(req.body.cpf)) return res.status(422).send("-1");
-
+		if (!validateName(name)) return res.status(422).send("-3");
+		if (!validateEmail(email)) return res.status(422).send("-2");
+		if (!validateCpf(cpf)) return res.status(422).send("-1");		
+		if (!!isDriver && !validateCarPlate(carPlate)) return res.status(422).send("-5");
 		
-		if (req.body.isDriver) {
-			if (req.body.carPlate.match(/[A-Z]{3}[0-9]{4}/)) {
-				await connection.query(
-					"insert into cccat16.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)",
-					[
-						id,
-						req.body.name,
-						req.body.email,
-						req.body.cpf,
-						req.body.carPlate,
-						!!req.body.isPassenger,
-						!!req.body.isDriver,
-					]
-				);
-				
-				return res.json({
-					accountId: id,
-				});
 
-			} else {
-				// invalid car plate
-				return res.status(422).send("-5");
-			}
-		} else {
-			await connection.query(
-				"insert into cccat16.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)",
-				[
-					id,
-					req.body.name,
-					req.body.email,
-					req.body.cpf,
-					req.body.carPlate,
-					!!req.body.isPassenger,
-					!!req.body.isDriver,
-				]
-			);
-			
-			return res.json({
-				accountId: id,
-			});
-		}
+		await connection.query(
+			"insert into cccat16.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)",
+			[
+				id,
+				req.body.name,
+				req.body.email,
+				req.body.cpf,
+				req.body.carPlate,
+				!!req.body.isPassenger,
+				!!req.body.isDriver,
+			]
+		);
+		
+		return res.json({
+			accountId: id,
+		});
+
+		// } 
   } finally {
     await connection.$pool.end();
   }
 });
+
+const validateName = (name: string) => {
+	return name.match(/[a-zA-Z] [a-zA-Z]+/);
+}
+
+const validateEmail = (email: string) => {
+	return email.match(/^(.+)@(.+)$/);
+}
+
+const validateCarPlate = (carPlate: string) => {
+	return carPlate.match(/[A-Z]{3}[0-9]{4}/);
+}
+
 
 app.listen(3000);
