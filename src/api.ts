a252215b156c83307d8e24cbee7ce0bbd4b40d9a
Bruno Russi Lautenschlager <brunoxd13@gmail.com>
2024-04-29 06:38:02 -0300

    split api, application

--- src/api.ts
+++ src/api.ts
@@ -1,79 +1,20 @@
-import { Request, Response, NextFunction } from 'express';
-import crypto from "crypto";
+import { Request, Response } from 'express';
 import express from "express";
-import pgp from "pg-promise";
-
-import { validate as validateCpf } from "./validateCpf";
+import { signUp } from './application';
 
 const app = express();
 app.use(express.json());
 
-const DATABASE_URL = process.env.DATABASE_URL || "postgres://postgres:123456@localhost:5432/app";
 
-const validateSignup = (req: Request, res: Response, next: NextFunction) => {
-	const { name, email, cpf, carPlate, isDriver } = req.body;
-
-	if (!validateName(name)) return res.status(422).send("Invalid name");
-	if (!validateEmail(email)) return res.status(422).send("Invalid email");
-	if (!validateCpf(cpf)) return res.status(422).send("Invalid CPF");
-	if (!!isDriver && !validateCarPlate(carPlate)) return res.status(422).send("Invalid car plate");
-
-	next();
-}
-
-app.post("/signup", validateSignup, async (req: Request, res: Response) => {
-  const { name, email, cpf, carPlate, isPassenger, isDriver } = req.body;
-	
-	if (await validateAccountExists(email)) return res.status(422).send("Account already exists");
-
-	const id = await createAccount(name, email, cpf, carPlate, isPassenger, isDriver);
-	return res.json({ accountId: id });
+app.post("/signup", async (req: Request, res: Response) => {
+  try {
+    const output = await signUp(req.body);
+    res.json(output);
+  } catch (error: any) {
+    res.status(422).json({
+      message: error.message
+    });
+  }
 });
-
-const validateAccountExists = async (email: string) => {
-  const connection = pgp()(DATABASE_URL);
-  try {
-    const [acc] = await connection.query(
-      "select * from cccat16.account where email = $1",
-      [email]
-    );
-    return !!acc;
-  } finally {
-    connection.$pool.end();
-  }
-};
-
-const validateName = (name: string) => {
-  return name.match(/[a-zA-Z] [a-zA-Z]+/);
-};
-
-const validateEmail = (email: string) => {
-  return email.match(/^(.+)@(.+)$/);
-};
-
-const validateCarPlate = (carPlate: string) => {
-  return carPlate.match(/[A-Z]{3}[0-9]{4}/);
-};
-
-const createAccount = async (
-  name: string,
-  email: string,
-  cpf: string,
-  carPlate: string,
-  isPassenger: boolean,
-  isDriver: boolean
-) => {
-  const connection = pgp()(DATABASE_URL);
-  try {
-    const id = crypto.randomUUID();
-    await connection.query(
-      "insert into cccat16.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)",
-      [id, name, email, cpf, carPlate, !!isPassenger, !!isDriver]
-    );
-    return id;
-  } finally {
-    await connection.$pool.end();
-  }
-};
 
 app.listen(3000);
